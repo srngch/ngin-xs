ad1a6ad39f34d7b4c5642c32d3e1e930bfc76df0
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-10-14 19:58:32 +0900

    chore: Edit main statement

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -13,20 +13,21 @@ int	main(int argc, char **argv, char **env) {
 		serverblocks = conf.getServerBlocks();
 		for (it = serverblocks.begin(); it != serverblocks.end(); it++)
 			it->printBlock();
-  } catch (std::exception &e) {
+	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
-      
-  try {
-	   Master master(env);
-     // print env
-     // char **envs = master.getEnv();
-     // while (envs && *envs) {
-     // 	std::cout << *envs << std::endl;
-     // 	envs++;
-     // }
-		 master.run();
-	   } catch (std::exception &e) {
-		   std::cout << e.what() << std::endl;
-	   }
+
+	try {
+		Master master(env);
+		// print env
+		// char **envs = master.getEnv();
+		// while (envs && *envs) {
+		// 	std::cout << *envs << std::endl;
+		// 	envs++;
+		// }
+		master.run();
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	return 0;
 }
